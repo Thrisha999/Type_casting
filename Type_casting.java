89516b1e79f6953898fa5ee2f48b9729ce0ad82a
Thrisha999 <101519219+Thrisha999@users.noreply.github.com>
2023-03-26 21:28:07 +0530

    Add files via upload

---
+++ Type_casting.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Type_casting
+{
+  public static void main(String[] args)
+  {
+     char c='a';
+     int n;
+     n=c;
+     System.out.println("num="+n);
+     float b=2.54f;
+     int d;
+     d=(int)b+1;
+     System.out.println("d="+d);
+     System.out.println("b="+b);
+  }
+}
